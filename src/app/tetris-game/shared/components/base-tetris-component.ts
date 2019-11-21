70bb13750be200aba173987e709b4538938bfd7d
Ji, Derek (DPTI) <Derek.Ji@sa.gov.au>
2019-11-21 16:50:12 +1030

    Tetris: Initial layout

---
+++ src/app/tetris-game/shared/components/base-tetris-component.ts
@@ -0,0 +1,8 @@
+import { BaseAppComponent } from '@shared/components/base-app.component';
+
+export abstract class BaseTetrisComponent extends BaseAppComponent {
+
+  constructor() {
+    super();
+  }
+}
